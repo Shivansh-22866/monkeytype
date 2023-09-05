ef9197a317a26552ea9d3b3fa4e18133e0d07722
Shivansh <shivanshp0418@gmail.com>
2023-09-05 13:53:53 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,62 @@
+import os
+import pyautogui
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+os.environ['PATH']
+driver = webdriver.Firefox()
+driver.get("https://monkeytype.com/")
+driver.set_window_position(0,0)
+driver.find_element(By.CLASS_NAME, "rejectAll").click()
+
+time.sleep(1)
+
+driver.find_element(By.CLASS_NAME, "view-settings").click()
+
+pyautogui.FAILSAFE = True
+
+time.sleep(1)
+
+pyautogui.click(x=995,y=380)
+
+time.sleep(1)
+
+driver.execute_script("window.scrollTo(0, 8400)")
+
+time.sleep(1)
+
+pyautogui.click(x=250,y=700)
+
+driver.execute_script("window.scrollTo(0,0)")
+
+time.sleep(1)
+
+driver.find_element(By.CLASS_NAME, "view-start").click()
+
+try:
+    while time.time() < time.time() + 30:
+        active_word = driver.find_element(By.CSS_SELECTOR, ".word.active")
+        letters = [letter.text for letter in active_word.find_elements(By.TAG_NAME, "letter")] + [' ']
+        pyautogui.write(letters, interval=0.0)
+except Exception as e:
+    pass
+    
+    
+time.sleep(2)
+
+driver.find_element(By.CLASS_NAME, "view-start").click()
+
+time.sleep(1)
+
+pyautogui.click(x=850,y=300)
+
+time.sleep(1)
+
+try:
+    while(driver.find_element(By.CSS_SELECTOR, ".word.active")):
+        active_word = driver.find_element(By.CSS_SELECTOR, ".word.active")
+        letters = [letter.text for letter in active_word.find_elements(By.TAG_NAME, "letter")] + [' ']
+        pyautogui.write(letters, interval=0.0)
+except Exception as e:
+    pass
