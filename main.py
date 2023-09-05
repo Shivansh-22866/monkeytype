a8552ded9b36aaa1c4dfbaffe978e10d99fe4f80
Shivansh <shivanshp0418@gmail.com>
2023-09-05 14:10:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,34 +4,37 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
+#setup the web browser
 os.environ['PATH']
 driver = webdriver.Firefox()
 driver.get("https://monkeytype.com/")
 driver.set_window_position(0,0)
+
+#Reject All Cookies
 driver.find_element(By.CLASS_NAME, "rejectAll").click()
-
 time.sleep(1)
 
+#Visit Settings
 driver.find_element(By.CLASS_NAME, "view-settings").click()
-
 pyautogui.FAILSAFE = True
-
 time.sleep(1)
 
+#Go to Themes
 pyautogui.click(x=995,y=380)
-
 time.sleep(1)
 
+#Scroll to theme options
 driver.execute_script("window.scrollTo(0, 8400)")
-
 time.sleep(1)
 
+#Click on the theme
 pyautogui.click(x=250,y=700)
 
+#Scroll back to top
 driver.execute_script("window.scrollTo(0,0)")
-
 time.sleep(1)
 
+#Start standard test
 driver.find_element(By.CLASS_NAME, "view-start").click()
 
 try:
@@ -45,12 +48,10 @@ except Exception as e:
     
 time.sleep(2)
 
+#Start test with medium length quotes
 driver.find_element(By.CLASS_NAME, "view-start").click()
-
 time.sleep(1)
-
 pyautogui.click(x=850,y=300)
-
 time.sleep(1)
 
 try:
